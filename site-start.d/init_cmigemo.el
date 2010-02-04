414d7efb5620004d51b814dc0284a592e54d8e25
sakito <sakito@sakito.com>
2010-02-05 05:41:44 +0900

    migemo の辞書をコミット

--- site-start.d/init_cmigemo.el
+++ site-start.d/init_cmigemo.el
@@ -27,7 +27,7 @@
 (setq migemo-command "cmigemo")
 (setq migemo-options '("-q" "--emacs" "-i" "\a"))
 ;; migemo-dict のパスを指定
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-dictionary "~/.emacs.d/etc/migemo/utf-8/migemo-dict")
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
 
