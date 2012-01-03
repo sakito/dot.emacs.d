c06277162245fede30adfa0a2c1ed679cc4661d0
sakito <sakito@sakito.com>
2012-01-04 08:43:03 +0900

    CL 用インストール設定を追加

--- rc.d/quicklisp-install.lisp
+++ rc.d/quicklisp-install.lisp
@@ -7,6 +7,11 @@
                 :clsql
                 :cl-fad
                 :series
+                :cl-csv
+                ;; ln -s /usr/local/mysql/lib/libmysqlclient.dylib ~/.ccl/quicklisp/dists/quicklisp/software/clsql-xxxx-git/db-mysql
+                :clsql-mysql
+                :hunchentoot
+                :hunchentoot-test
                 ))
 
 (ql:add-to-init-file)
