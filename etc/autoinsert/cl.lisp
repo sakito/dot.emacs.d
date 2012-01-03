385d1c89b182c6c5f0f7963b0a64ad0f249a4db3
sakito <sakito@sakito.com>
2012-01-03 11:03:09 +0900

    lisp 用の autoinsert を設定

---
+++ etc/autoinsert/cl.lisp
@@ -0,0 +1,11 @@
+#|
+Copyright (C) %year% sakito <sakito@sakito.com>
+
+All parts of this program are licensed under the terms of the LLGPL
+unless otherwise noted.
+|#
+
+(in-package :cl-user)
+(defpackage %file%
+  (:use :cl))
+(in-package :%file%)
--- site-start.d/init_autoinsert.el
+++ site-start.d/init_autoinsert.el
@@ -2,7 +2,7 @@
 
 ;;; init_autoinsert.el --- auto-insert setting
 
-;; Copyright (C) 2004  sakito
+;; Copyright (C) 2004-2012  sakito
 
 ;; Author: sakito <sakito@sakito.com>
 
@@ -21,49 +21,25 @@
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-;;; Commentary: auto-insertの設定
+;;; Commentary:
 
-;; 
+;;  auto-insertの設定
 
 ;;; Code:
 
 (require 'autoinsert)
+
+;; ファイルを開いたら実行
 (add-hook 'find-file-hooks 'auto-insert)
+
 ;; テンプレートとなるファイルがあるディレクトリ
 ;; 末尾に"/"が必要なので注意
 (setq auto-insert-directory "~/.emacs.d/etc/autoinsert/")
+
 ;; 質問しないで auto-insertを実行する
 (setq auto-insert-query nil)
 
-;; XMLへの追加
-(setq auto-insert-alist
-      (append '(
-                (xml-mode . "xml-insert.xml")
-                )
-              auto-insert-alist))
-
-;; Texinfoへの追加
-(setq auto-insert-alist
-      (append '(
-                (texinfo-mode . "texinfo.texi")
-                )
-              auto-insert-alist))
-
-;;(add-to-list 'auto-insert-alist '(emacs-lisp-mode  . "skelton.el"))
-(add-to-list 'auto-insert-alist '(perl-mode . "perl.pl"))
-(add-to-list 'auto-insert-alist '(cperl-mode . "perl.pl"))
-;;(add-to-list 'auto-insert-alist '(python-mode . "python.py"))
-;;(add-to-list 'auto-insert-alist '(ruby-mode . "skelton.rb"))
-;;(add-to-list 'auto-insert-alist '("¥¥.tex¥¥'" . "tex-insert.tex"))
-;;(add-to-list 'auto-insert-alist '("¥¥.html¥¥'" . "html4trans.html"))
-
-(setq auto-insert-alist
-      (nconc '(
-               ("\\.rst$" . ["rst.rst" my-template])
-               (python-mode . ["python.py" my-template])
-               ) auto-insert-alist))
-
-
+;; テンプレート用の置換文字列
 (defvar template-replacements-alists
   '(
     ("%file%"             . (lambda () (file-name-nondirectory (buffer-file-name))))
@@ -74,68 +50,54 @@
 ;    ("%include-guard%"    . (lambda () (format "__SCHEME_%s__" (upcase (file-name-sans-extension (file-name-nondirectory buffer-file-name))))))
     ))
 
+
+;; 置換用の関数
 (defun my-template ()
   (time-stamp)
   (mapc #'(lambda(c)
             (progn
               (goto-char (point-min))
-              (replace-string (car c) (funcall (cdr c)) nil)))
+              (replace-string (first c) (funcall (rest c)) nil)))
         template-replacements-alists)
   (goto-char (point-max))
   (message "done."))
 (add-hook 'find-file-not-found-hooks 'auto-insert)
 
+;; 各モードの設定
 
-;; (defun my-insert-texinfo-template ()
-;;   (let*
-;;       (
-;;        (title (read-string "title: "))
-;;        (myname (concat user-full-name))
-;;        (filename (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
-;;        (current-date (current-time-string))
-;;        )
-;;     (progn
-;;       (insert
-;;        "\\input texinfo @c -*-texinfo-*-"
-;;        "\n@c %**start of header"
-;;        "\n@setfilename " filename ".info"
-;;        "\n@settitle " title
-;;        "\n@setchapternewpage odd"
-;;        "\n@c %**end of header"
-;;        "\n"
-;;        "\n@c $Id$"
-;;        "\n"
-;;        "@today{}\n"
-;;        "\n"
-;;        "@titlepage\n"
-;;        "@title @author @page\n"
-;;        "@end titlepage\n"
-;;        "@node Top, Front Matter, (dir), (dir)\n"
-;;        "\n"
-;;        "@top " title "\n"
-;;        "\n"
-;;        "@ifinfo\n"
-;;        "@menu\n"
-;;        "* Front Matter::\n"
-;;        "@end menu\n"
-;;        "\n"
-;;        "@node Front Matter\n"
-;;        "@unnumbered Front Matter\n"
-;;        "\n"
-;;        "@end ifinfo\n"
-;;        "\n"
-;;        "@contents\n"
-;;        "@bye\n"
-;;        )
-;;       (beginning-of-buffer)
-;;       )))
+;; XML
+(setq auto-insert-alist
+      (append '((xml-mode . "xml-insert.xml"))
+              auto-insert-alist))
 
-;; (setq auto-insert-alist
-;;       (append
-;;        '(
-;;          (texinfo-mode . my-insert-texinfo-template)
-;;          )
-;;       auto-insert-alist))
+;; Texinfo
+(setq auto-insert-alist
+      (append '((texinfo-mode . "texinfo.texi"))
+              auto-insert-alist))
+
+
+;; Perl
+(add-to-list 'auto-insert-alist '(perl-mode . "perl.pl"))
+(add-to-list 'auto-insert-alist '(cperl-mode . "perl.pl"))
+
+;; Python
+(setq auto-insert-alist
+      (nconc '(
+               ("\\.rst$" . ["rst.rst" my-template])
+               (python-mode . ["python.py" my-template])
+               ) auto-insert-alist))
+
+;; Lisp
+(setq auto-insert-alist
+      (nconc '(
+               ("\\.cl$" . ["cl.lisp" my-template])
+               ("\\.lisp$" . ["cl.lisp" my-template])
+               (lisp-mode . ["cl.lisp" my-template])
+               ) auto-insert-alist))
+
+;;(add-to-list 'auto-insert-alist '(emacs-lisp-mode  . "skelton.el"))
+;;(add-to-list 'auto-insert-alist '(python-mode . "python.py"))
+
 
 (provide 'init_autoinsert)
 ;;; init_autoinsert.el ends here
