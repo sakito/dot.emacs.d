3502ac788bc166fe914fb0d4970e0db602c4adc7
sakito <sakito@sakito.com>
2012-01-05 22:37:41 +0900

    anything を最新版に更新

--- local-lisp/anything/anything-match-plugin.el
+++ local-lisp/anything/anything-match-plugin.el
@@ -1,9 +1,12 @@
-;;; anything-match-plugin.el --- Humane match plug-in for anything
+;;; anything-match-plugin.el --- Multiple regexp matching methods for anything
 
 ;; Author: rubikitch <rubikitch@ruby-lang.org>
 
-;; Copyright (C) 2008~2011  rubikitch, all rights reserved.
-;; Copyright (C) 2011, Thierry Volpiatto, all rights reserved.
+;; Maintainers: rubikitch <rubikitch@ruby-lang.org>
+;;              Thierry Volpiatto <thierry.volpiatto@gmail.com>
+
+;; Copyright (C) 2008~2012, rubikitch, all rights reserved.
+;; Copyright (C) 2011~2012, Thierry Volpiatto, all rights reserved.
 
 ;; Keywords: anything, matching
 ;; X-URL: <http://repo.or.cz/w/anything-config.git>
@@ -22,6 +25,85 @@
 ;; along with GNU Emacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
+
+;;; Auto documentation
+;;  ------------------
+;;
+;;  * User variables
+;; [EVAL] (autodoc-document-lisp-buffer :type 'user-variable :prefix "anything-mp" :var-value t)
+;; `anything-mp-matching-method'
+;; Default Value: multi3
+;;
+;;  * Internal variables
+;; [EVAL] (autodoc-document-lisp-buffer :type 'internal-variable :prefix "anything-mp" :var-value t)
+;; `anything-mp-default-match-functions'
+;; Default Value: (anything-mp-exact-match anything-mp-3-match) 
+;; `anything-mp-default-search-functions'
+;; Default Value: (anything-mp-exact-search anything-mp-3-search) 
+;; `anything-mp-default-search-backward-functions'
+;; Default Value: (anything-mp-exact-search-backward anything-mp-3-search-backward) 
+;; `anything-mp-space-regexp'
+;; Default Value: "[\\ ] "
+;; `anything-mp-exact-pattern-str'
+;; Default Value: "autodoc"
+;; `anything-mp-exact-pattern-real'
+;; Default Value: "\nautodoc\n"
+;; `anything-mp-prefix-pattern-str'
+;; Default Value: "find"
+;; `anything-mp-prefix-pattern-real'
+;; Default Value: "\nfind"
+;; `anything-mp-1-pattern-str'
+;; Default Value: nil
+;; `anything-mp-1-pattern-real'
+;; Default Value: nil
+;; `anything-mp-2-pattern-str'
+;; Default Value: "lisp ini"
+;; `anything-mp-2-pattern-real'
+;; Default Value: "^.*lisp.*ini"
+;; `anything-mp-3-pattern-str'
+;; Default Value: "autodoc"
+;; `anything-mp-3-pattern-list'
+;; Default Value: ((identity . "autodoc")) 
+;; `anything-mp-highlight-delay'
+;; Default Value: 0.7
+;; `anything-mp-highlight-threshold'
+;; Default Value: 2
+;;
+;;  * Anything match plugin Functions
+;; [EVAL] (autodoc-document-lisp-buffer :type 'function :prefix "anything-mp")
+;; `anything-mp-set-matching-method'
+;; `anything-mp-make-regexps'
+;; `anything-mp-1-make-regexp'
+;; `anything-mp-exact-get-pattern'
+;; `anything-mp-exact-match'
+;; `anything-mp-exact-search'
+;; `anything-mp-exact-search-backward'
+;; `anything-mp-prefix-get-pattern'
+;; `anything-mp-prefix-match'
+;; `anything-mp-prefix-search'
+;; `anything-mp-prefix-search-backward'
+;; `anything-mp-1-get-pattern'
+;; `anything-mp-1-match'
+;; `anything-mp-1-search'
+;; `anything-mp-1-search-backward'
+;; `anything-mp-2-get-pattern'
+;; `anything-mp-2-match'
+;; `anything-mp-2-search'
+;; `anything-mp-2-search-backward'
+;; `anything-mp-3-get-patterns'
+;; `anything-mp-3-get-patterns-internal'
+;; `anything-mp-3-match'
+;; `anything-mp-3-search-base'
+;; `anything-mp-3-search'
+;; `anything-mp-3-search-backward'
+;; `anything-mp-3p-match'
+;; `anything-mp-3p-search'
+;; `anything-mp-3p-search-backward'
+;; `anything-mp-highlight-match'
+;; `anything-mp-highlight-region'
+;; `anything-mp-highlight-match-internal'
+
+;;  *** END auto-documentation
 
 ;;; Commentary:
 
@@ -69,16 +151,99 @@
 (require 'anything)
 (require 'cl)
 
-
+
 ;;;; Match-plugin
 
-;;; multiple patterns
+;; Internal
+(defvar anything-mp-default-match-functions nil)
+(defvar anything-mp-default-search-functions nil)
+(defvar anything-mp-default-search-backward-functions nil)
+
+(defun anything-mp-set-matching-method (var key)
+  "Default function to set matching methods in anything match plugin."
+  (set-default var key)
+  (case (symbol-value var)
+    (multi1 (setq anything-mp-default-match-functions
+                  '(anything-mp-exact-match anything-mp-1-match)
+                  anything-mp-default-search-functions
+                  '(anything-mp-exact-search anything-mp-1-search)
+                  anything-mp-default-search-backward-functions
+                  '(anything-mp-exact-search-backward
+                    anything-mp-1-search-backward)))
+    (multi2 (setq anything-mp-default-match-functions
+                  '(anything-mp-exact-match anything-mp-2-match)
+                  anything-mp-default-search-functions
+                  '(anything-mp-exact-search anything-mp-2-search)
+                  anything-mp-default-search-backward-functions
+                  '(anything-mp-exact-search-backward
+                    anything-mp-2-search-backward)))
+    (multi3 (setq anything-mp-default-match-functions
+                  '(anything-mp-exact-match anything-mp-3-match)
+                  anything-mp-default-search-functions
+                  '(anything-mp-exact-search anything-mp-3-search)
+                  anything-mp-default-search-backward-functions
+                  '(anything-mp-exact-search-backward
+                    anything-mp-3-search-backward)))
+    (multi3p (setq anything-mp-default-match-functions
+                   '(anything-mp-exact-match anything-mp-3p-match)
+                   anything-mp-default-search-functions
+                   '(anything-mp-exact-search anything-mp-3p-search)
+                   anything-mp-default-search-backward-functions
+                   '(anything-mp-exact-search-backward
+                     anything-mp-3p-search-backward)))
+    (t (error "Unknow value: %s" anything-mp-matching-method))))
+
+(defgroup anything-match-plugin nil
+  "Anything match plugin."
+  :group 'anything)
+
+(defcustom anything-mp-matching-method 'multi3
+  "Matching method for anything match plugin.
+You can set here different methods to match candidates in anything.
+Here are the possible value of this symbol and their meaning:
+- multi1: Respect order, prefix of pattern must match.
+- multi2: Same but with partial match.
+- multi3: The best, multiple regexp match, allow negation.
+- multi3p: Same but prefix must match.
+Default is multi3."
+  :type  '(radio :tag "Matching methods for anything"
+           (const :tag "Multiple regexp 1 ordered with prefix match"         multi1)
+           (const :tag "Multiple regexp 2 ordered with partial match"        multi2)
+           (const :tag "Multiple regexp 3 matching no order, partial, best." multi3)
+           (const :tag "Multiple regexp 3p matching with prefix match"       multi3p))
+  :set   'anything-mp-set-matching-method
+  :group 'anything-match-plugin)
+
+(defface anything-match
+    '((t (:inherit match)))
+  "Face used to highlight matches."
+  :group 'anything-match-plugin)
+
+(defcustom anything-mp-highlight-delay 0.7
+  "Highlight matches with `anything-match' face after this many seconds.
+ If nil, no highlight. "
+  :type  'integer
+  :group 'anything-match-plugin)
+
+(defcustom anything-mp-highlight-threshold 2
+  "Minimum length of pattern to highlight.
+The smaller  this value is, the slower highlight is."
+  :type  'integer
+  :group 'anything-match-plugin)
+
+
+
+;;; Build regexps
 ;;
 ;;
 (defvar anything-mp-space-regexp "[\\ ] "
   "Regexp to represent space itself in multiple regexp match.")
 
 (defun anything-mp-make-regexps (pattern)
+  "Split PATTERN if it contain spaces and return resulting list.
+If spaces in PATTERN are escaped, don't split at this place.
+i.e \"foo bar\"=> (\"foo\" \"bar\")
+but \"foo\ bar\"=> (\"foobar\")."
   (if (string= pattern "")
       '("")
       (loop for s in (split-string
@@ -88,11 +253,14 @@
             collect (replace-regexp-in-string "\000\000" " " s))))
 
 (defun anything-mp-1-make-regexp (pattern)
+  "Replace spaces in PATTERN with \"\.*\"."
   (mapconcat 'identity (anything-mp-make-regexps pattern) ".*"))
 
+
 ;;; Exact match.
 ;;
 ;;
+;; Internal.
 (defvar anything-mp-exact-pattern-str nil)
 (defvar anything-mp-exact-pattern-real nil)
 
@@ -114,10 +282,11 @@
   (and (search-backward (anything-mp-exact-get-pattern pattern) nil t)
        (forward-line 1)))
 
-
+
 ;;; Prefix match
 ;;
 ;;
+;; Internal
 (defvar anything-mp-prefix-pattern-str nil)
 (defvar anything-mp-prefix-pattern-real nil)
 
@@ -140,9 +309,11 @@
   (and (search-backward (anything-mp-prefix-get-pattern pattern) nil t)
        (forward-line 1)))
 
+
 ;;; Multiple regexp patterns 1 (order is preserved / prefix).
 ;;
 ;;
+;; Internal
 (defvar anything-mp-1-pattern-str nil)
 (defvar anything-mp-1-pattern-real nil)
 
@@ -162,9 +333,11 @@
 (defun anything-mp-1-search-backward (pattern &rest ignore)
   (re-search-backward (anything-mp-1-get-pattern pattern) nil t))
 
+
 ;;; Multiple regexp patterns 2 (order is preserved / partial).
 ;;
 ;;
+;; Internal
 (defvar anything-mp-2-pattern-str nil)
 (defvar anything-mp-2-pattern-real nil)
 
@@ -172,7 +345,7 @@
   (unless (equal pattern anything-mp-2-pattern-str)
     (setq anything-mp-2-pattern-str pattern
           anything-mp-2-pattern-real
-          (concat "^.+" (anything-mp-1-make-regexp pattern))))
+          (concat "^.*" (anything-mp-1-make-regexp pattern))))
   anything-mp-2-pattern-real)
 
 (defun* anything-mp-2-match (str &optional (pattern anything-pattern))
@@ -184,9 +357,11 @@
 (defun anything-mp-2-search-backward (pattern &rest ignore)
   (re-search-backward (anything-mp-2-get-pattern pattern) nil t))
 
+
 ;;; Multiple regexp patterns 3 (permutation).
 ;;
 ;;
+;; Internal
 (defvar anything-mp-3-pattern-str nil)
 (defvar anything-mp-3-pattern-list nil)
 
@@ -209,11 +384,20 @@ e.g ((identity . \"foo\") (identity . \"bar\"))."
                       (cons 'not (substring pat 1))
                       (cons 'identity pat)))))
 
-(defun* anything-mp-3-match (str &optional (pattern anything-pattern))
-  (when (stringp pattern)
-    (setq pattern (anything-mp-3-get-patterns pattern)))
-  (loop for (predicate . regexp) in pattern
-        always (funcall predicate (string-match regexp str))))
+(defun anything-mp-3-match (str &optional pattern)
+  "Check if PATTERN match STR.
+When PATTERN contain a space, it is splitted and matching is done
+with the several resulting regexps against STR.
+e.g \"bar foo\" will match \"foobar\" and \"barfoo\".
+Argument PATTERN, a string, is transformed in a list of
+cons cell with `anything-mp-3-get-patterns' if it contain a space.
+e.g \"foo bar\"=>((identity . \"foo\") (identity . \"bar\")).
+Then each predicate of cons cell(s) is called with regexp of same
+cons cell against STR (a candidate).
+i.e (identity (string-match \"foo\" \"foo bar\")) => t."
+  (let ((pat (anything-mp-3-get-patterns (or pattern anything-pattern))))
+    (loop for (predicate . regexp) in pat
+          always (funcall predicate (string-match regexp str)))))
 
 (defun anything-mp-3-search-base (pattern searchfn1 searchfn2)
   (loop with pat = (if (stringp pattern)
@@ -240,10 +424,22 @@ e.g ((identity . \"foo\") (identity . \"bar\"))."
     (setq pattern (anything-mp-3-get-patterns pattern)))
   (anything-mp-3-search-base
    pattern 're-search-backward 're-search-backward))
-  
+
+  
 ;;; mp-3p- (multiple regexp pattern 3 with prefix search)
 ;;
 ;;
+(defun anything-mp-3p-match (str &optional pattern)
+  "Check if PATTERN match STR.
+Same as `anything-mp-3-match' but more strict, matching against prefix also.
+e.g \"bar foo\" will match \"barfoo\" but not \"foobar\" contrarily to
+`anything-mp-3-match'."
+  (let* ((pat (anything-mp-3-get-patterns (or pattern anything-pattern)))
+         (first (car pat)))
+    (and (funcall (car first) (anything-mp-prefix-match str (cdr first)))
+         (loop for (predicate . regexp) in (cdr pat)
+               always (funcall predicate (string-match regexp str))))))
+  
 (defun anything-mp-3p-search (pattern &rest ignore)
   (when (stringp pattern)
     (setq pattern (anything-mp-3-get-patterns pattern)))
@@ -256,23 +452,10 @@ e.g ((identity . \"foo\") (identity . \"bar\"))."
   (anything-mp-3-search-base
    pattern 'anything-mp-prefix-search-backward 're-search-backward))
 
+
 ;;; source compiler
 ;;
 ;;
-(defvar anything-mp-default-match-functions
-  '(anything-mp-exact-match
-    anything-mp-3-match))
-
-(defvar anything-mp-default-search-functions
-  '(anything-mp-exact-search
-    anything-mp-3p-search
-    anything-mp-3-search))
-
-(defvar anything-mp-default-search-backward-functions
-  '(anything-mp-exact-search-backward
-    anything-mp-3p-search-backward
-    anything-mp-3-search-backward))
-
 (defun anything-compile-source--match-plugin (source)
   (let ((searchers (if (assoc 'search-from-end source)
                        anything-mp-default-search-backward-functions
@@ -287,22 +470,10 @@ e.g ((identity . \"foo\") (identity . \"bar\"))."
        ,@source)))
 (add-to-list 'anything-compile-source-functions 'anything-compile-source--match-plugin t)
 
+
 ;;; Highlight matches.
 ;;
 ;;
-(defface anything-match
-    '((t (:inherit match)))
-  "Face used to highlight matches."
-  :group 'anything)
-
-(defvar anything-mp-highlight-delay 0.7
-  "Highlight matches with `anything-match' face after this many seconds.
- If nil, no highlight. ")
-
-(defvar anything-mp-highlight-threshold 2
-  "Minimum length of pattern to highlight.
-The smaller  this value is, the slower highlight is.")
-
 (defun anything-mp-highlight-match ()
   "Highlight matches after `anything-mp-highlight-delay' seconds."
   (when (and anything-mp-highlight-delay
@@ -320,21 +491,66 @@ The smaller  this value is, the slower highlight is.")
       (while (and (setq me (re-search-forward regexp nil t))
                   (< (point) end)
                   (< 0 (- (match-end 0) (match-beginning 0))))
-        (put-text-property (match-beginning 0) me 'face face)))))
+        (unless (anything-pos-header-line-p)
+          (put-text-property (match-beginning 0) me 'face face))))))
 
-(defun* anything-mp-highlight-match-internal (end)
+(defun anything-mp-highlight-match-internal (end)
   (when (anything-window)
     (set-buffer anything-buffer)
-    (let ((requote (regexp-quote anything-pattern)))
+    (let ((requote (loop for (pred . re) in
+                         (anything-mp-3-get-patterns anything-pattern)
+                         when (and (eq pred 'identity)
+                                    (>= (length re)
+                                        anything-mp-highlight-threshold))
+                         collect re into re-list
+                         finally return
+                         (if (and re-list (>= (length re-list) 1))
+                             (mapconcat 'identity re-list "\\|")
+                             (regexp-quote anything-pattern)))))
       (when (>= (length requote) anything-mp-highlight-threshold)
-        (anything-mp-highlight-region (point-min) end
-                                      requote 'anything-match)))
-    (loop for (pred . re) in (anything-mp-3-get-patterns anything-pattern)
-          when (and (eq pred 'identity)
-                    (>= (length re) anything-mp-highlight-threshold))
-          do
-          (anything-mp-highlight-region (point-min) end re 'anything-match))))
+        (anything-mp-highlight-region
+         (point-min) end requote 'anything-match)))))
 
+
+;;; Toggle anything-match-plugin
+;;
+;;
+(defvar anything-mp-initial-highlight-delay nil)
+
+;;;###autoload
+(defun anything-mp-toggle-match-plugin ()
+  "Turn on/off multiple regexp matching in anything.
+i.e anything-match-plugin."
+  (interactive)
+  (let ((anything-match-plugin-enabled
+         (member 'anything-compile-source--match-plugin
+                 anything-compile-source-functions)))
+    (flet ((disable-match-plugin ()
+             (setq anything-compile-source-functions
+                   (delq 'anything-compile-source--match-plugin
+                         anything-compile-source-functions))
+             (setq anything-mp-initial-highlight-delay
+                   anything-mp-highlight-delay)
+             (setq anything-mp-highlight-delay nil))
+           (enable-match-plugin ()
+             (unless anything-mp-initial-highlight-delay
+               (setq anything-mp-initial-highlight-delay
+                     anything-mp-highlight-delay))
+             (setq anything-compile-source-functions
+                   (cons 'anything-compile-source--match-plugin
+                         anything-compile-source-functions))
+             (unless anything-mp-highlight-delay
+               (setq anything-mp-highlight-delay
+                     anything-mp-initial-highlight-delay))))
+      (if anything-match-plugin-enabled
+          (when (y-or-n-p "Really disable match-plugin? ")
+            (disable-match-plugin)
+            (message "Anything-match-plugin disabled"))
+          (when (y-or-n-p "Really enable match-plugin? ")
+            (enable-match-plugin)
+            (message "Anything-match-plugin enabled"))))))
+
+
 ;;;; Grep-candidates plug-in
 
 (defcustom anything-grep-candidates-fast-directory-regexp nil
