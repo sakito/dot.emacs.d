3f7c13edb38e9bff160fb8ededc2395c48febc48
sakito <sakito@sakito.com>
2012-01-14 11:24:19 +0900

    python-mode 6.0.4 に更新

--- lisp/python-mode/anything-ipython.el
+++ lisp/python-mode/anything-ipython.el
@@ -100,7 +100,7 @@
 (setq ipython-completion-command-string "print(';'.join(__IP.Completer.all_completions('%s')))\n")
 
 (defadvice ipython-shell-hook (after unset-completion-key () activate)
-  (define-key py-mode-map (kbd "M-<tab>") 'anything-ipython-complete))
+  (define-key python-mode-map (kbd "M-<tab>") 'anything-ipython-complete))
 
 ;; Modify original `ipython-complete' to fit with anything.
 (defun anything-ipython-completion-list (pattern)
