be0b2dd6c790938ac87dccd356c1940bd2acf03d
sakito <sakito@sakito.com>
2012-07-21 21:12:27 +0900

    auto-complete update

--- lisp/auto-complete/auto-complete-config.el
+++ lisp/auto-complete/auto-complete-config.el
@@ -141,17 +141,21 @@
 
 (defun ac-yasnippet-candidates ()
   (with-no-warnings
-      (if (fboundp 'yas/get-snippet-tables)
-          ;; >0.6.0
-          (apply 'append (mapcar 'ac-yasnippet-candidate-1 (yas/get-snippet-tables major-mode)))
-        (let ((table
-               (if (fboundp 'yas/snippet-table)
-                   ;; <0.6.0
-                   (yas/snippet-table major-mode)
-                 ;; 0.6.0
-                 (yas/current-snippet-table))))
-          (if table
-              (ac-yasnippet-candidate-1 table))))))
+    (if (fboundp 'yas/get-snippet-tables)
+        ;; >0.6.0
+        (apply 'append (mapcar 'ac-yasnippet-candidate-1
+                               (condition-case nil
+                                   (yas/get-snippet-tables major-mode)
+                                 (wrong-number-of-arguments
+                                  (yas/get-snippet-tables)))))
+      (let ((table
+             (if (fboundp 'yas/snippet-table)
+                 ;; <0.6.0
+                 (yas/snippet-table major-mode)
+               ;; 0.6.0
+               (yas/current-snippet-table))))
+        (if table
+            (ac-yasnippet-candidate-1 table))))))
 
 (ac-define-source yasnippet
   '((depends yasnippet)
