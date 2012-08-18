de7aac0b157090a250002c8ff128a593d738621a
sakito <sakito@sakito.com>
2012-08-18 23:15:05 +0900

    d-mode update

--- lisp/d-mode/d-mode.el
+++ lisp/d-mode/d-mode.el
@@ -3,21 +3,22 @@
 
 ;; Author:     2007 William Baxter
 ;; Contributors: Andrei Alexandrescu
-;; Maintainer: William Baxter
+;; Contributors: Russel Winder
+;; Maintainer: Russel Winder
 ;; Created:    March 2007
-;; Version:    2.0.4 (February 2008)
+;; Version:    2.0.5-SNAPSHOT (2010-11-13)
 ;; Keywords:   D programming language emacs cc-mode
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2 of the License, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
@@ -35,7 +36,7 @@
 ;; Commentary:
 ;;   This mode supports most of D's syntax, including nested /+ +/
 ;;   comments and backquote `string literals`.
-;;   
+;;
 ;;   This mode has been dubbed "2.0" because it is a complete rewrite
 ;;   from scratch.  The previous d-mode was based on cc-mode 5.28 or
 ;;   so.  This version is based on the cc-mode 5.30 derived mode
@@ -49,6 +50,7 @@
 ;;     individually.  It didn't seem to work right though.
 ;;
 ;; History:
+;;   * 2010-11-13 -- 2.0.5-SNAPSHOT add immutable as a keyword.
 ;;   * 2008 February - 2.0.4 - fixed "else static if" indentation problem, 
 ;;      and also a problem with "debug if()" indentation.
 ;;      Some D2 additions (invariant as type modifier etc).
@@ -114,8 +116,14 @@
   "List of the tokens made up of characters in the punctuation or
 parenthesis syntax classes that have uses other than as expression
 operators."
+
+;  Emergency Emacs 23 fixes from http://www.prowiki.org/wiki4d/wiki.cgi?EditorSupport/EmacsDMode
+
+;  d (append '("/+" "+/" "..." ".." "!" "*" "&")
+;       (c-lang-const c-other-op-syntax-tokens)))
+
   d '("/+" "+/" "..." ".." "!" "*" "&"))
-  
+
 (c-lang-defconst c-block-comment-starter d "/*")
 (c-lang-defconst c-block-comment-ender   d "*/")
 
@@ -134,27 +142,28 @@ operators."
 
 ;; Built-in basic types
 (c-lang-defconst c-primitive-type-kwds
-  d '("bit" "byte" "ubyte" "char" "delegate" "double" "float" "function" 
-      "int" "long" "ubyte" "short" "uint" "ulong" "ushort" "cent" "ucent" 
-      "real" "ireal" "ifloat" "creal" "cfloat" "cdouble"
-      "wchar" "dchar" "void"))
+  d '("bit" "bool" "byte" "ubyte" "char" "delegate" "double" "float"
+      "function" "int" "long" "ubyte" "short" "uint" "ulong" "ushort"
+      "cent" "ucent" "real" "ireal" "ifloat" "creal" "cfloat" "cdouble"
+      "wchar" "dchar" "void" "string" "wstring" "dstring"))
 
 ;; Keywords that can prefix normal declarations of identifiers
 (c-lang-defconst c-modifier-kwds
-  d '("auto" "abstract" "const" "deprecated" "extern" 
-      "final" "lazy" "private" "protected" "public"
-      "scope" "static" "synchronized" "volatile" "mixin"))
+  d '("__gshared" "auto" "abstract" "const" "deprecated" "extern"
+      "final" "immutable" "inout" "lazy" "mixin" "private" "protected"
+      "public" "scope" "shared" "static" "synchronized" "volatile" ))
 
 (c-lang-defconst c-class-decl-kwds
   ;; Keywords introducing declarations where the following block (if any)
   ;; contains another declaration level that should be considered a class.
-  d '("class" "struct" "union" "interface" "template"))
+  d '("class" "struct" "union" "interface"))
 
 ;; (c-lang-defconst c-brace-list-decl-kwds
 ;;   d '("enum"))
 
 (c-lang-defconst c-type-modifier-kwds
-  d '("const" "lazy" "volatile" "invariant" "enum")
+  d '("__gshared" "const" "immutable" "inout" "lazy" "shared" "volatile"
+      "invariant" "enum")
 )
 (c-lang-defconst c-type-prefix-kwds
   ;; Keywords where the following name - if any - is a type name, and
@@ -173,7 +182,13 @@ operators."
 ;;  d '("with" "version" "extern"))
 
 (c-lang-defconst c-typedef-decl-kwds
-  d '("typedef" "alias"))
+
+;  Emergency Emacs 23 fixes from http://www.prowiki.org/wiki4d/wiki.cgi?EditorSupport/EmacsDMode
+
+;  d (append (c-lang-const c-typedef-decl-kwds)
+;       '("typedef" "alias")))
+
+   d '("typedef" "alias"))
 
 (c-lang-defconst c-decl-hangon-kwds
   d '("export"))
@@ -262,13 +277,13 @@ operators."
 
 (c-lang-defconst c-other-kwds
   ;; Keywords not accounted for by any other `*-kwds' language constant.
-  d '("assert"))
+  d '("__gshared" "__traits" "assert" "cast" "is" "nothrow" "pure" "ref"
+      "sizeof" "template" "typeid" "typeof"))
 
 
 (defcustom d-font-lock-extra-types nil
   "*List of extra types (aside from the type keywords) to recognize in D mode.
-   Each list item should be a regexp matching a single identifier."
-  :group "D")
+   Each list item should be a regexp matching a single identifier.")
 
 (defconst d-font-lock-keywords-1 (c-lang-const c-matchers-1 d)
   "Minimal highlighting for D mode.")
@@ -286,13 +301,13 @@ operators."
   "Syntax table used in d-mode buffers.")
 (or d-mode-syntax-table
     (setq d-mode-syntax-table
-	 (let ((table (funcall (c-lang-const c-make-mode-syntax-table d))))
-	   ;; Make it recognize D `backquote strings`
-	   (modify-syntax-entry ?` "\"" table)
+          (let ((table (funcall (c-lang-const c-make-mode-syntax-table d))))
+            ;; Make it recognize D `backquote strings`
+            (modify-syntax-entry ?` "\"" table)
 
-	   ;; Make it recognize D's nested /+ +/ comments 
-	   (modify-syntax-entry ?+  ". 23n"   table)
-	   table)))
+            ;; Make it recognize D's nested /+ +/ comments 
+            (modify-syntax-entry ?+  ". 23n"   table)
+            table)))
 
 (defvar d-mode-abbrev-table nil
   "Abbreviation table used in d-mode buffers.")
@@ -322,7 +337,7 @@ operators."
   t `(["Comment Out Region"     comment-region
        (c-fn-region-is-active-p)]
       ["Uncomment Region"       (comment-region (region-beginning)
-						(region-end) '(4))
+                                                (region-end) '(4))
        (c-fn-region-is-active-p)]
       ["Indent Expression"      c-indent-exp
        (memq (char-after) '(?\( ?\[ ?\{))]
@@ -334,22 +349,23 @@ operators."
       "----"
       ("Toggle..."
        ["Syntactic indentation" c-toggle-syntactic-indentation
-	:style toggle :selected c-syntactic-indentation]
+        :style toggle :selected c-syntactic-indentation]
        ["Electric mode"         c-toggle-electric-state
-	:style toggle :selected c-electric-flag]
+        :style toggle :selected c-electric-flag]
        ["Auto newline"          c-toggle-auto-newline
-	:style toggle :selected c-auto-newline]
+        :style toggle :selected c-auto-newline]
        ["Hungry delete"         c-toggle-hungry-state
-	:style toggle :selected c-hungry-delete-key]
+        :style toggle :selected c-hungry-delete-key]
        ["Subword mode"          c-subword-mode
-	:style toggle :selected (and (boundp 'c-subword-mode)
+        :style toggle :selected (and (boundp 'c-subword-mode)
                                      c-subword-mode)])))
 
 (easy-menu-define d-menu d-mode-map "D Mode Commands"
   (cons "D" (c-lang-const c-mode-menu d)))
 
 ;;----------------------------------------------------------------------------
-;;;###autoload (add-to-list 'auto-mode-alist '("\\.d[i]?\\'" . d-mode))
+;;;###autoload
+(add-to-list 'auto-mode-alist (cons (purecopy "\\.d[i]?\\'")  'd-mode))
 
 ;;;###autoload
 (defun d-mode ()
@@ -365,9 +381,9 @@ Key bindings:
   (c-initialize-cc-mode t)
   (set-syntax-table d-mode-syntax-table)
   (setq major-mode 'd-mode
-	mode-name "D"
-	local-abbrev-table d-mode-abbrev-table
-	abbrev-mode t)
+        mode-name "D"
+        local-abbrev-table d-mode-abbrev-table
+        abbrev-mode t)
   (use-local-map d-mode-map)
   (c-init-language-vars d-mode)
   (c-common-init 'd-mode)
