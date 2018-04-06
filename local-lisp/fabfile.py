3c7f128ef9b9c0bae217f73ec8f8d85681a250e4
sakito <sakito@sakito.com>
2018-04-06 14:42:42 +0900

    fabfileの設定を修正

--- local-lisp/fabfile.py
+++ local-lisp/fabfile.py
@@ -13,6 +13,7 @@ from fabric.api import (
     lcd,
 )
 
+
 @task
 def up():
     """
@@ -30,5 +31,26 @@ def up():
                     local('hg pull; hg up')
                 elif os.path.exists('{}/.git'.format(dir_name)):
                     local('git pull; git fetch')
+                else:
+                    local('pwd')
+
+
+@task
+def st():
+    """
+    確認
+    """
+    print('status')
+    dir_list = dircache.listdir('.')
+    for dir_name in dir_list:
+        if os.path.isdir(dir_name):
+            print('----------')
+            print(dir_name)
+
+            with lcd(dir_name):
+                if os.path.exists('{}/.hg'.format(dir_name)):
+                    local('hg st')
+                elif os.path.exists('{}/.git'.format(dir_name)):
+                    local('git incoming')
                 else:
                     local('pwd')
