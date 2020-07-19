eb27c7730926fe7cd59cfa38f26d43ba3b6794e9
sakito <sakito@sakito.com>
2020-07-19 09:04:22 +0900

    カレンダーに週番号表示

--- site-start.d/init_calendar.el
+++ site-start.d/init_calendar.el
@@ -31,18 +31,36 @@
 (define-key calendar-mode-map "f" 'calendar-forward-day)
 (define-key calendar-mode-map "n" 'calendar-forward-day)
 (define-key calendar-mode-map "b" 'calendar-backward-day)
-;; 祝日をマークする
-(setq calendar-mark-holidays-flag t)
+
+;; week number
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%02dW"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian
+                   (list month (- day (1- calendar-week-start-day)) year)))))
+        'font-lock-face 'calendar-iso-week-face))
+
+;; https://github.com/emacs-jp/japanese-holidays
 (require 'japanese-holidays)
+;; 他の国の祝日も表示させたい場合は適当に調整
 (setq calendar-holidays
       (append japanese-holidays holiday-local-holidays holiday-other-holidays))
-;; 今日をマークする
-(add-hook 'today-visible-calendar-hook 'calendar-mark-today)
-;; 日曜日を赤字にする
-(setq calendar-weekend-marker 'diary)
-(add-hook 'today-visible-calendar-hook 'calendar-mark-weekend)
-(add-hook 'today-invisible-calendar-hook 'calendar-mark-weekend)
 
+;; 祝日をカレンダーに表示
+(setq calendar-mark-holidays-flag t)
+
+;; 土曜日・日曜日を祝日として表示する
+;; デフォルトで設定済み
+;; (setq japanese-holiday-weekend '(0 6)     ;; 土日を祝日として表示
+;;       japanese-holiday-weekend-marker    ;;  土曜日を水色で表示
+;;       '(holiday nil nil nil nil nil japanese-holiday-saturday))
+
+;; 今日をマークする
+(add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+(add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
+(add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
 
 (provide 'init_calendar)
 ;;; init_calendar.el ends here
